ae328a136170f3ad043fd388da6f560ac05ab4a3
Prasanth Raghavan <mailprasanthtr@gmail.com>
2017-10-29 09:15:54 +0530

    Currency converter basic elements

---
+++ src/app/components/Currencies.js
@@ -0,0 +1,493 @@
+export const CURRENCIES = [{
+  "value": "USD",
+  "name": "US Dollar"
+}, {
+  "value": "INR",
+  "name": "Indian Rupee"
+}, {
+  "value": "AFA",
+  "name": "Afghan Afghani (1927–2002)"
+}, {
+  "value": "ALL",
+  "name": "Albanian Lek"
+}, {
+  "value": "DZD",
+  "name": "Algerian Dinar"
+}, {
+  "value": "AOA",
+  "name": "Angolan Kwanza"
+}, {
+  "value": "ARS",
+  "name": "Argentine Peso"
+}, {
+  "value": "AMD",
+  "name": "Armenian Dram"
+}, {
+  "value": "AWG",
+  "name": "Aruban Florin"
+}, {
+  "value": "AUD",
+  "name": "Australian Dollar"
+}, {
+  "value": "AZN",
+  "name": "Azerbaijani Manat"
+}, {
+  "value": "BSD",
+  "name": "Bahamian Dollar"
+}, {
+  "value": "BHD",
+  "name": "Bahraini Dinar"
+}, {
+  "value": "BDT",
+  "name": "Bangladeshi Taka"
+}, {
+  "value": "BBD",
+  "name": "Barbadian Dollar"
+}, {
+  "value": "BYR",
+  "name": "Belarusian Ruble (2000–2016)"
+}, {
+  "value": "BEF",
+  "name": "Belgian Franc"
+}, {
+  "value": "BZD",
+  "name": "Belize Dollar"
+}, {
+  "value": "BMD",
+  "name": "Bermudan Dollar"
+}, {
+  "value": "BTN",
+  "name": "Bhutanese Ngultrum"
+}, {
+  "value": "BTC",
+  "name": "Bitcoin"
+}, {
+  "value": "BOB",
+  "name": "Bolivian Boliviano"
+}, {
+  "value": "BAM",
+  "name": "Bosnia-Herzegovina Convertible Mark"
+}, {
+  "value": "BWP",
+  "name": "Botswanan Pula"
+}, {
+  "value": "BRL",
+  "name": "Brazilian Real"
+}, {
+  "value": "GBP",
+  "name": "British Pound"
+}, {
+  "value": "BND",
+  "name": "Brunei Dollar"
+}, {
+  "value": "BGN",
+  "name": "Bulgarian Lev"
+}, {
+  "value": "BIF",
+  "name": "Burundian Franc"
+}, {
+  "value": "KHR",
+  "name": "Cambodian Riel"
+}, {
+  "value": "CAD",
+  "name": "Canadian Dollar"
+}, {
+  "value": "CVE",
+  "name": "Cape Verdean Escudo"
+}, {
+  "value": "KYD",
+  "name": "Cayman Islands Dollar"
+}, {
+  "value": "XAF",
+  "name": "Central African CFA Franc"
+}, {
+  "value": "XPF",
+  "name": "CFP Franc"
+}, {
+  "value": "CLP",
+  "name": "Chilean Peso"
+}, {
+  "value": "CNY",
+  "name": "Chinese Yuan"
+}, {
+  "value": "COP",
+  "name": "Colombian Peso"
+}, {
+  "value": "KMF",
+  "name": "Comorian Franc"
+}, {
+  "value": "CDF",
+  "name": "Congolese Franc"
+}, {
+  "value": "CRC",
+  "name": "Costa Rican Colón"
+}, {
+  "value": "HRK",
+  "name": "Croatian Kuna"
+}, {
+  "value": "CUC",
+  "name": "Cuban Convertible Peso"
+}, {
+  "value": "CZK",
+  "name": "Czech Koruna"
+}, {
+  "value": "DKK",
+  "name": "Danish Krone"
+}, {
+  "value": "DJF",
+  "name": "Djiboutian Franc"
+}, {
+  "value": "DOP",
+  "name": "Dominican Peso"
+}, {
+  "value": "XCD",
+  "name": "East Caribbean Dollar"
+}, {
+  "value": "EGP",
+  "name": "Egyptian Pound"
+}, {
+  "value": "ERN",
+  "name": "Eritrean Nakfa"
+}, {
+  "value": "EEK",
+  "name": "Estonian Kroon"
+}, {
+  "value": "ETB",
+  "name": "Ethiopian Birr"
+}, {
+  "value": "EUR",
+  "name": "Euro"
+}, {
+  "value": "FKP",
+  "name": "Falkland Islands Pound"
+}, {
+  "value": "FJD",
+  "name": "Fijian Dollar"
+}, {
+  "value": "GMD",
+  "name": "Gambian Dalasi"
+}, {
+  "value": "GEL",
+  "name": "Georgian Lari"
+}, {
+  "value": "DEM",
+  "name": "German Mark"
+}, {
+  "value": "GHS",
+  "name": "Ghanaian Cedi"
+}, {
+  "value": "GIP",
+  "name": "Gibraltar Pound"
+}, {
+  "value": "GRD",
+  "name": "Greek Drachma"
+}, {
+  "value": "GTQ",
+  "name": "Guatemalan Quetzal"
+}, {
+  "value": "GNF",
+  "name": "Guinean Franc"
+}, {
+  "value": "GYD",
+  "name": "Guyanaese Dollar"
+}, {
+  "value": "HTG",
+  "name": "Haitian Gourde"
+}, {
+  "value": "HNL",
+  "name": "Honduran Lempira"
+}, {
+  "value": "HKD",
+  "name": "Hong Kong Dollar"
+}, {
+  "value": "HUF",
+  "name": "Hungarian Forint"
+}, {
+  "value": "ISK",
+  "name": "Icelandic Króna"
+}, {
+  "value": "IDR",
+  "name": "Indonesian Rupiah"
+}, {
+  "value": "IRR",
+  "name": "Iranian Rial"
+}, {
+  "value": "IQD",
+  "name": "Iraqi Dinar"
+}, {
+  "value": "ILS",
+  "name": "Israeli New Shekel"
+}, {
+  "value": "ITL",
+  "name": "Italian Lira"
+}, {
+  "value": "JMD",
+  "name": "Jamaican Dollar"
+}, {
+  "value": "JPY",
+  "name": "Japanese Yen"
+}, {
+  "value": "JOD",
+  "name": "Jordanian Dinar"
+}, {
+  "value": "KZT",
+  "name": "Kazakhstani Tenge"
+}, {
+  "value": "KES",
+  "name": "Kenyan Shilling"
+}, {
+  "value": "KWD",
+  "name": "Kuwaiti Dinar"
+}, {
+  "value": "KGS",
+  "name": "Kyrgystani Som"
+}, {
+  "value": "LAK",
+  "name": "Laotian Kip"
+}, {
+  "value": "LVL",
+  "name": "Latvian Lats"
+}, {
+  "value": "LBP",
+  "name": "Lebanese Pound"
+}, {
+  "value": "LSL",
+  "name": "Lesotho Loti"
+}, {
+  "value": "LRD",
+  "name": "Liberian Dollar"
+}, {
+  "value": "LYD",
+  "name": "Libyan Dinar"
+}, {
+  "value": "LTL",
+  "name": "Lithuanian Litas"
+}, {
+  "value": "MOP",
+  "name": "Macanese Pataca"
+}, {
+  "value": "MKD",
+  "name": "Macedonian Denar"
+}, {
+  "value": "MGA",
+  "name": "Malagasy Ariary"
+}, {
+  "value": "MWK",
+  "name": "Malawian Kwacha"
+}, {
+  "value": "MYR",
+  "name": "Malaysian Ringgit"
+}, {
+  "value": "MVR",
+  "name": "Maldivian Rufiyaa"
+}, {
+  "value": "MRO",
+  "name": "Mauritanian Ouguiya"
+}, {
+  "value": "MUR",
+  "name": "Mauritian Rupee"
+}, {
+  "value": "MXN",
+  "name": "Mexican Peso"
+}, {
+  "value": "MDL",
+  "name": "Moldovan Leu"
+}, {
+  "value": "MNT",
+  "name": "Mongolian Tugrik"
+}, {
+  "value": "MAD",
+  "name": "Moroccan Dirham"
+}, {
+  "value": "MZM",
+  "name": "Mozambican Metical (1980–2006)"
+}, {
+  "value": "MMK",
+  "name": "Myanmar Kyat"
+}, {
+  "value": "NAD",
+  "name": "Namibian Dollar"
+}, {
+  "value": "NPR",
+  "name": "Nepalese Rupee"
+}, {
+  "value": "ANG",
+  "name": "Netherlands Antillean Guilder"
+}, {
+  "value": "TWD",
+  "name": "New Taiwan Dollar"
+}, {
+  "value": "NZD",
+  "name": "New Zealand Dollar"
+}, {
+  "value": "NIO",
+  "name": "Nicaraguan Córdoba"
+}, {
+  "value": "NGN",
+  "name": "Nigerian Naira"
+}, {
+  "value": "KPW",
+  "name": "North Korean Won"
+}, {
+  "value": "NOK",
+  "name": "Norwegian Krone"
+}, {
+  "value": "OMR",
+  "name": "Omani Rial"
+}, {
+  "value": "PKR",
+  "name": "Pakistani Rupee"
+}, {
+  "value": "PAB",
+  "name": "Panamanian Balboa"
+}, {
+  "value": "PGK",
+  "name": "Papua New Guinean Kina"
+}, {
+  "value": "PYG",
+  "name": "Paraguayan Guarani"
+}, {
+  "value": "PEN",
+  "name": "Peruvian Sol"
+}, {
+  "value": "PHP",
+  "name": "Philippine Peso"
+}, {
+  "value": "PLN",
+  "name": "Polish Zloty"
+}, {
+  "value": "QAR",
+  "name": "Qatari Rial"
+}, {
+  "value": "RON",
+  "name": "Romanian Leu"
+}, {
+  "value": "RUB",
+  "name": "Russian Ruble"
+}, {
+  "value": "RWF",
+  "name": "Rwandan Franc"
+}, {
+  "value": "SVC",
+  "name": "Salvadoran Colón"
+}, {
+  "value": "WST",
+  "name": "Samoan Tala"
+}, {
+  "value": "SAR",
+  "name": "Saudi Riyal"
+}, {
+  "value": "RSD",
+  "name": "Serbian Dinar"
+}, {
+  "value": "SCR",
+  "name": "Seychellois Rupee"
+}, {
+  "value": "SLL",
+  "name": "Sierra Leonean Leone"
+}, {
+  "value": "SGD",
+  "name": "Singapore Dollar"
+}, {
+  "value": "SKK",
+  "name": "Slovak Koruna"
+}, {
+  "value": "SBD",
+  "name": "Solomon Islands Dollar"
+}, {
+  "value": "SOS",
+  "name": "Somali Shilling"
+}, {
+  "value": "ZAR",
+  "name": "South African Rand"
+}, {
+  "value": "KRW",
+  "name": "South Korean Won"
+}, {
+  "value": "XDR",
+  "name": "Special Drawing Rights"
+}, {
+  "value": "LKR",
+  "name": "Sri Lankan Rupee"
+}, {
+  "value": "SHP",
+  "name": "St. Helena Pound"
+}, {
+  "value": "SDG",
+  "name": "Sudanese Pound"
+}, {
+  "value": "SRD",
+  "name": "Surinamese Dollar"
+}, {
+  "value": "SZL",
+  "name": "Swazi Lilangeni"
+}, {
+  "value": "SEK",
+  "name": "Swedish Krona"
+}, {
+  "value": "CHF",
+  "name": "Swiss Franc"
+}, {
+  "value": "SYP",
+  "name": "Syrian Pound"
+}, {
+  "value": "STD",
+  "name": "São Tomé &amp; Príncipe Dobra"
+}, {
+  "value": "TJS",
+  "name": "Tajikistani Somoni"
+}, {
+  "value": "TZS",
+  "name": "Tanzanian Shilling"
+}, {
+  "value": "THB",
+  "name": "Thai Baht"
+}, {
+  "value": "TOP",
+  "name": "Tongan Paʻanga"
+}, {
+  "value": "TTD",
+  "name": "Trinidad &amp; Tobago Dollar"
+}, {
+  "value": "TND",
+  "name": "Tunisian Dinar"
+}, {
+  "value": "TRY",
+  "name": "Turkish Lira"
+}, {
+  "value": "TMT",
+  "name": "Turkmenistani Manat"
+}, {
+  "value": "UGX",
+  "name": "Ugandan Shilling"
+}, {
+  "value": "UAH",
+  "name": "Ukrainian Hryvnia"
+}, {
+  "value": "AED",
+  "name": "United Arab Emirates Dirham"
+}, {
+  "value": "UYU",
+  "name": "Uruguayan Peso"
+}, {
+  "value": "UZS",
+  "name": "Uzbekistani Som"
+}, {
+  "value": "VUV",
+  "name": "Vanuatu Vatu"
+}, {
+  "value": "VEF",
+  "name": "Venezuelan Bolívar"
+}, {
+  "value": "VND",
+  "name": "Vietnamese Dong"
+}, {
+  "value": "XOF",
+  "name": "West African CFA Franc"
+}, {
+  "value": "YER",
+  "name": "Yemeni Rial"
+}, {
+  "value": "ZMK",
+  "name": "Zambian Kwacha (1968–2012)"
+}];
--- src/app/components/CurrencyConverter.js
+++ src/app/components/CurrencyConverter.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+import { CURRENCIES } from './Currencies';
+
 export default class CurrencyConverter extends React.Component {
   render() {
+    const currencyOptions = CURRENCIES.map((currency, index) => {
+      return <option value={currency.value} key={index}>{currency.name}</option>
+    });
+
     return (
       <div>
-          <h1>CurrencyConverter</h1>
+        <div>
+          <label>$100 equals Rs. 6400/-</label>
+        </div>
+        <div>
+          <select name="fromCurrency">{currencyOptions}</select>
+          to
+          <select name="fromCurrency">{currencyOptions}</select>
+        </div>
+        <div>
+          <input/>
+        </div>
       </div>
       );
   }
