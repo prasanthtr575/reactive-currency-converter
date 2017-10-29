25471e1ae6c8e2cee403a0034dcd0ff36ee994d9
Prasanth Raghavan <mailprasanthtr@gmail.com>
2017-10-29 10:54:46 +0530

    Currency convert is more reactive

--- src/app/components/CurrencyConverter.js
+++ src/app/components/CurrencyConverter.js
@@ -3,6 +3,44 @@ import React from "react";
 import { CURRENCIES } from './Currencies';
 
 export default class CurrencyConverter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fromCurrency: "USD",
+      toCurrency: "INR",
+      moneyValue: 0
+    }
+  }
+
+  changeFromCurrency(event) {
+    let currency = event.target.value;
+
+    this.setState({
+      fromCurrency: currency
+    });
+  }
+
+  changeToCurrency(event) {
+    let currency = event.target.value;
+
+    this.setState({
+      toCurrency: currency
+    });
+  }
+
+  changeMoneyValue(event) {
+    let moneyValue = event.target.value;
+
+    this.setState({
+      moneyValue: moneyValue
+    });
+  }
+
+  convertCurrency(money, fromCurrency, toCurrency) {
+    let convertedMoney = 7777;
+    return convertedMoney;
+  }
+
   render() {
     const currencyOptions = CURRENCIES.map((currency, index) => {
       return <option value={currency.value} key={index}>{currency.name}</option>
@@ -11,15 +49,28 @@ export default class CurrencyConverter extends React.Component {
     return (
       <div>
         <div>
-          <label>$100 equals Rs. 6400/-</label>
+          <label>
+            {this.state.moneyValue} {this.state.fromCurrency} equals {
+      this.convertCurrency(
+        this.state.moneyValue,
+        this.state.fromCurrency,
+        this.state.toCurrency
+      )
+      } {this.state.toCurrency}</label>
         </div>
         <div>
-          <select name="fromCurrency">{currencyOptions}</select>
+          <select
+      name="fromCurrency"
+      onChange={this.changeFromCurrency.bind(this)}
+      value={this.state.fromCurrency}> {currencyOptions} </select>
           to
-          <select name="fromCurrency">{currencyOptions}</select>
+          <select
+      name="fromCurrency"
+      onChange={this.changeToCurrency.bind(this)}
+      value={this.state.toCurrency}> {currencyOptions} </select>
         </div>
         <div>
-          <input/>
+          <input value={this.state.moneyValue} onChange={this.changeMoneyValue.bind(this)}/>
         </div>
       </div>
       );
